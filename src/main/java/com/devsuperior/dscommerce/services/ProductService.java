d31c04ca3dedf90e6aa7517e7411ea549c84fd72
luizffdemoraes <lffm1994@gmail.com>
2024-12-09 23:28:42 -0300

    Criando DTO e estruturando camadas

---
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -0,0 +1,22 @@
+package com.devsuperior.dscommerce.services;
+
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public ProductDTO findById(Long id) {
+        Optional<Product> result = repository.findById(1L);
+        Product product = result.get();
+        return new ProductDTO(product);
+    }
+}
