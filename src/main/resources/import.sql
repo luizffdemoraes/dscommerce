1214fda5b275712b24a465642a2a0b0f2161e3a3
luizffdemoraes <lffm1994@gmail.com>
2025-01-27 22:35:27 -0300

    Adicionando segurança ao DSCommerce

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,7 @@
 INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Maria Brown', 'maria@gmail.com', '988888888', '$2a$10$4L.RbGRaiT5Xts27T1d0quYf.Ih.w3qewVZd.RrrINu76lUxICnBi', '2001-07-25');
 INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Alex Green', 'alex@gmail.com', '977777777', '$2a$10$4L.RbGRaiT5Xts27T1d0quYf.Ih.w3qewVZd.RrrINu76lUxICnBi', '1987-12-13');
 
-INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_CLIENT');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
